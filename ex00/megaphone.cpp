959781ebeca2d8afaab5a45ce5e211a93ad6ce47
Efe balık <efebalikofc@hotmail.com>
2023-11-07 20:33:01 +0300

    megaphone argc

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,17 +2,21 @@
 
 int main(int argc, char const *argv[])
 {
-    (void)argc;
     argv++;
-    while(*argv)
+    if (argc >= 2)
     {
-        while (**argv)
+        while(*argv)
         {
-            std::cout << (char)toupper(**argv);
-            (*argv)++;
+            while (**argv)
+            {
+                std::cout << (char)toupper(**argv);
+                (*argv)++;
+            }
+            argv++;
         }
-        argv++;
     }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::cout << "\n";
     return 0;
 }
